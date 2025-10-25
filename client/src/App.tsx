25623eb74dff331cf124e2add6d7326da8342afd
Josue <Josue>
2025-10-25 01:54:06 -0600

    fixed add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,8 +27,8 @@ function Router() {
       <ProtectedRoute path="/experiences/:id" component={ExperienceDetailPage} />
   <ProtectedRoute path="/friends" component={FriendsPage} />
   <ProtectedRoute path="/adventures" component={AdventuresPage} />
-  <ProtectedRoute path="/adventures/:id" component={AdventureDetailPage} />
   <ProtectedRoute path="/adventures/create" component={CreateAdventurePage} />
+  <ProtectedRoute path="/adventures/:id" component={AdventureDetailPage} />
       <ProtectedRoute path="/profile" component={ProfilePage} />
       <Route component={NotFound} />
     </Switch>
